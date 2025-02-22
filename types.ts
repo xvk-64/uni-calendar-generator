2d21d7a92210bce18af86cfccaa7969999841299
xvk-64 <18544814+xvk-64@users.noreply.github.com>
2025-02-22 23:46:23 +1030

    Initial commit

---
+++ types.ts
@@ -0,0 +1,66 @@
+export type DateTimeRange = { start: string; end: string };
+
+type WeekDay = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
+
+type Meeting = {
+    day: WeekDay;
+    location: string;
+    date: DateTimeRange;
+    time: DateTimeRange;
+};
+export type Meetings = Array<Meeting>;
+export type CourseName = {
+    subject: string;
+    code: string;
+    title: string;
+};
+
+export type Course = {
+    id: string;
+    course_id: string;
+    name: CourseName;
+    class_number: number;
+    year: number;
+    term: string;
+    campus: string;
+    units: number;
+    requirements: unknown;
+    class_list: Array<{
+        id: string;
+        category: 'enrolment' | 'related';
+        type: string;
+        classes: Array<{
+            number: string;
+            meetings: Meetings;
+        }>;
+    }>;
+};
+
+export type DetailedEnrolledCourse = {
+    id: string;
+    name: CourseName;
+    classes: Array<{
+        typeId: string;
+        type: string;
+        classNumber: string;
+        meetings: Meetings;
+    }>;
+};
+
+export type WeekCourse = {
+    id: string;
+    name: CourseName;
+    classTypeId: string;
+    classType: string;
+    location: string;
+    classNumber: string;
+};
+export type WeekCourses = Array<
+    Array<{ time: DateTimeRange; courses: Array<WeekCourse> }>
+>;
+
+export type OtherWeekCourseTime = {
+    classes: Array<{ number: string; location: string }>;
+    time: DateTimeRange;
+};
+export type OtherWeekCoursesTimes = Array<Array<OtherWeekCourseTime>>;
